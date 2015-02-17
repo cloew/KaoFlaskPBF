6d97a1347e726e4fbc69865d3de2ea2f0b984cdb
Chris Loew <cloew123@gmail.com>
2015-02-17 10:16:30 -0700

    Added command to copy the server init

---
+++ pbf_kao_flask/Commands/new_server.py
@@ -0,0 +1,20 @@
+from pbf.templates.template_loader import TemplateLoader
+from pbf_kao_flask.templates import TemplatesRoot
+
+class NewServer:
+    """ Command to create a new Server """
+    TEMPLATE_LOADER = TemplateLoader("server_init.py", TemplatesRoot, defaultFilename="__init__.py")
+                          
+    def addArguments(self, parser):
+        """ Add arguments to the parser """
+        parser.add_argument('filepath', action='store', help='Path to Filename to copy template to')
+    
+    def run(self, arguments):
+        """ Run the command """
+        filepath = arguments.filepath
+        self.createServer(filepath)
+        
+    def createServer(self, filepath):
+        """ Create a Server """
+        keywords = {}
+        self.TEMPLATE_LOADER.copy(filepath, keywords=keywords)
